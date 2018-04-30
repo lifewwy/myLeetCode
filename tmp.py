0cc751a4cd52e38b96b5a5c52e01261046219ae4
lifewwy <lifewwy@gmail.com>
2018-04-30 14:41:35 -0400

    文件更新

--- tmp.py
+++ tmp.py
@@ -1,52 +1,23 @@
 
-def println(l, N = 10):
-    print(l.val, end='\t')
-    cursor = l.next
-    nCount = 1
-    while cursor != None and nCount < N:
-        nCount += 1
-        print(cursor.val, end='\t')
-        cursor = cursor.next
-    print()
-
-
-# Definition for singly-linked list.
-class ListNode(object):
-    def __init__(self, x):
-        self.val = x
-        self.next = None
-
-
-head = ListNode(1)
-head.next = ListNode(2)
-head.next.next = ListNode(3)
-
-println(head)
-
-
-print('head的地址：', id(head))
-print('head.next的地址：', id(head.next))
-
-
-# head.next, head = head, head.next
-# head, head.next = head.next, head
-
-
-print('head的地址：', id(head))
-print('head.next的地址：', id(head.next))
-
-println(head)
 
 
 
+def sum_recu(n):
+    if n > 0:
+        return n + sum_recu( n - 1 )
+    else:
+        return 0
 
+print("递归求和：",sum_recu(4))
 
+def func(n):
+    print('a', end='\t')
 
+    if n<= 1:
+        return
 
+    func(n-1)
 
+    print('b', end='\t')
 
-
-
-
-
-
+func(3)
